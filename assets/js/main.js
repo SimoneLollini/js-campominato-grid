d4b4e6850929c695fbca9e60ecfe81f138ee7915
SimoneLollini <lollinisimone@gmail.com>
2022-10-18 11:52:13 +0200

    add class active on click

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,25 +2,52 @@
 // Ogni cella ha un numero progressivo, da 1 a 100.
 // Ci saranno quindi 10 caselle per ognuna delle 10 righe.
 // Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
-const btn = document.querySelector('button')
-const field = document.querySelector('.container.field')
-let cells = 100;
+// const btn = document.querySelector('button')
+// const fieldSelector = document.querySelector('.container.field')
+
+
+// const cellsNumber = 100;
+// btn.addEventListener('click', function () {
+//     btn.classList.add('d_none')
+
+//     cellsGenerator(cellsNumber, fieldSelector)
+//     // const cellElementSelection = document.querySelectorAll('cell')   
+// })
+
+// let arrayCell = [];
+// let cellElement = '';
+// let i = 0;
+// function cellsGenerator(cellsNumber, parentElement) {
+//     for (let i = 0; i < cellsNumber; i++) {
+//         cellElement = `<div class="cell"> ${i + 1} </div>`
+//         parentElement.insertAdjacentHTML('beforeend', cellElement)  
+//     }
+// }
+
+const field = document.querySelector('.field');
+const btn = document.querySelector('button');
+
 btn.addEventListener('click', function () {
-    // console.log('Hai cliccato!');
-    btn.classList.add('d_none')
-    cellsGenerator(cells, field)
-})
+    field.innerHTML = '';
+    const cellNumber = 100;
+    generateCells(field, cellNumber);
+});
 
-function cellsGenerator(cellsNumber, parentElement) {
+
+function generateCells(parentElement, cellsNumber) {
     for (let i = 0; i < cellsNumber; i++) {
+        const cellElement = document.createElement('div');
+        cellElement.className = 'cell';
+        cellElement.innerText = i + 1;
+        parentElement.append(cellElement);
+    }
 
-        parentElement.insertAdjacentHTML('beforeend', `<div class="cell"> ${i + 1} </div>`)
-
+    const cellSelector = document.querySelectorAll('.cell');
+    for (let i = 0; i < cellSelector.length; i++) {
+        const singleCell = cellSelector[i]
+        singleCell.addEventListener('click', function () {
+            singleCell.classList.add('active');
+            console.log(singleCell.innerHTML);
+        })
     }
 }
-/* 
-for (let i = 0; i < cells; i++) {
-    console.log(i);
-    field.insertAdjacentHTML('beforeend', `<div class="cell"> ${i + 1} </div>`)
-}
-*/
